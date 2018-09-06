e4fcd745dc0b5dfbf2cc58b88f7ed89f023b1560
David Sabine <DavidSabine@users.noreply.github.com>
2018-09-06 17:04:09 -0400

    The system now builds
    It doesn't display properly in a browser, unfortunately.  :(  must debug

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
-export const plugins = [
-  {
-    resolve: `gatsby-plugin-emotion`,
-    options: {}
-  }
-]
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      }
+    }
+  ]
+};
