265700a7f692a0d97c5ad4785da38cb6f0c7978d
David Sabine <DavidSabine@users.noreply.github.com>
2018-09-08 12:42:25 -0400

    Added 404 e2e test and implemented <Link /> on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import { Link } from "gatsby"
 
 class FourOhFour extends React.Component {
   render() {
@@ -6,7 +7,7 @@ class FourOhFour extends React.Component {
       <div>
         <h1>Page not found</h1>
         <p>
-          You might find what you're looking for at <a href="/">the index page</a>.
+          You might find what you're looking for at <Link to="/">the index page</Link>.
         </p>
       </div>
     );
