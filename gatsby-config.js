d7630cd138c2643cb19644ff3006732b26c54776
David Sabine <DavidSabine@users.noreply.github.com>
2018-11-01 16:30:52 -0400

    Added gatsby-plugin-manifest
    And ensured that 'gatsby-plugin-offline' is *after* it in package.json (as instructed)

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,16 +6,29 @@ module.exports = {
         // Accepts all options defined by `babel-plugin-emotion` plugin.
       }
     },
-    `gatsby-plugin-offline`,
 		{
-			resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				anonymize: true,
+        anonymize: true,
 				exclude: ["/preview/**", "/do-not-track/me/too/"],
 				head: true,
 				respectDNT: true,
 				trackingId: `UA-125425021-1`,
 			}
 		},
+		{
+      resolve: `gatsby-plugin-manifest`,
+			options: {
+        background_color: `#242943`,
+				display: `minimal-ui`,
+				icon: `src/images/512px-Home_font_awesome.svg.png`,
+				name: `Gatsby Starter Hello World for TDD`,
+				orientation: `any`,
+				short_name: `Hello World`,
+				start_url: `/`,
+				theme_color: `#ffffff`,
+			}
+		},
+    `gatsby-plugin-offline`,
   ]
 };
