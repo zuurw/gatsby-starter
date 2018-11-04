3df99fed7fd70058bbd7bf66e1b29f959ebc5ca7
David Sabine <DavidSabine@users.noreply.github.com>
2018-11-03 21:51:53 -0400

    Refactored typographyjs tests

--- cypress/integration/typography.e2e.js
+++ cypress/integration/typography.e2e.js
@@ -1,14 +1,16 @@
-describe("typography.js", () => {
-	it("is used in index page", () => {
-		cy.request(`/`)
-		.should((xhr) => {
-			expect(xhr.body).to.contain(`typography.js`)
-		})
-	})
-	it("is used in 404 error page", () => {
-		cy.request(`/404/`)
-		.should((xhr) => {
-			expect(xhr.body).to.contain(`typography.js`)
+describe(`typography.js`, () => {
+
+	const urls = [
+		`/`,
+		`/404/`
+	]
+
+	urls.forEach(element => {
+		it(`is used in page: `+ element, () => {
+			cy.request(element)
+			.should((xhr) => {
+				expect(xhr.body).to.contain(`typography.js`)
+			})
 		})
 	})
 })
